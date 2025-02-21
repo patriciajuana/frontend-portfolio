0d9344d4f94393d5d7bf3a52587049877c6fa1c6
patrick <patrick@progeny.tech>
2025-02-21 09:39:52 -0500

    progress: icons

--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,20 @@ import PortfolioView from '@/views/PortfolioView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition // Restore scroll position when using browser back/forward
+    } else {
+      return { top: 0, behavior: 'smooth' } // Scroll to top on route change
+    }
+  },
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      //component: () => import('../views/AboutView.vue'),
-    },
+    //Listing Page
     {
       path: '/portfolios/:filterBy?',
       name: 'portfolios',
@@ -28,21 +28,21 @@ const router = createRouter({
         filters: ['web-app', 'wordpress', 'design'],
       }),
     },
-    //Web Apps
+    //Single Page Web Apps
     {
       path: '/:id(kyc-web-app|kyc-admin|radium)',
       name: 'web-apps-portfolio',
       component: PortfolioView,
       props: (route) => ({ id: route.params.id }),
     },
-    //WordPress
+    //Single Page WordPress
     {
       path: '/:id(blackhawk-tire-website|sailun-tire-website|cypressfuneral-website|magiline-website)',
       name: 'wordpress-portfolio',
       component: PortfolioView,
       props: (route) => ({ id: route.params.id }),
     },
-    //Design
+    //Single Page Design
     {
       path: '/:id(album-release-package|restaurant-menu|technical-drawing|movie-poster|mobile-app)',
       name: 'design-portfolio',
